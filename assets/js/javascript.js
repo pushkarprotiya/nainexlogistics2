f0479d50015395b8f235b3ca74d38fcecbbef58f
[pushkarprotiya] <[pushkarprotiya@gmail.com]>
2023-10-12 17:32:18 +0530

    Add all my files

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,14 +1,5 @@
 // AOS JS
 
-
-$(window).on('load', function () { // makes sure the whole site is loaded 
-  $('#status').fadeOut(); // will first fade out the loading animation 
-  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-  $('body').delay(350).css({ 'overflow': 'visible' });
-})
-
-
-
 const body = document.querySelector("body");
 const cursor = document.querySelector("#cursor");
 
@@ -16,3 +7,50 @@ body.addEventListener("mousemove", (dets) => {
   cursor.style.left = `${dets.x + 10}px`;
   cursor.style.top = `${dets.y + 10}px`;
 });
+
+
+
+
+$(function() {
+  $('marquee').mouseover(function() {
+      $(this).attr('scrollamount',0);
+  }).mouseout(function() {
+       $(this).attr('scrollamount',0);
+  });
+});
+
+
+
+
+(function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 3,
+          nav: true
+        }
+      }
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
